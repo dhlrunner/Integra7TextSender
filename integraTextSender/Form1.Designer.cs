29db8bd4621ee261f6bfe9defeacbf8709e7c4e9
도형 이 <dhlrunner@outlook.com>
2022-11-08 15:53:34 +0900

    Add project files.

---
+++ integraTextSender/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace integraTextSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numChan = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxAutoSend = new System.Windows.Forms.CheckBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.cmbMididev = new System.Windows.Forms.ComboBox();
+            this.llblSysEx = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.numChan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numChan
+            // 
+            this.numChan.Enabled = false;
+            this.numChan.Location = new System.Drawing.Point(27, 29);
+            this.numChan.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numChan.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numChan.Name = "numChan";
+            this.numChan.Size = new System.Drawing.Size(51, 20);
+            this.numChan.TabIndex = 0;
+            this.numChan.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Channel";
+            // 
+            // tbxText
+            // 
+            this.tbxText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxText.Enabled = false;
+            this.tbxText.Location = new System.Drawing.Point(104, 29);
+            this.tbxText.Name = "tbxText";
+            this.tbxText.Size = new System.Drawing.Size(438, 20);
+            this.tbxText.TabIndex = 2;
+            this.tbxText.TextChanged += new System.EventHandler(this.tbxText_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Text";
+            // 
+            // cbxAutoSend
+            // 
+            this.cbxAutoSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxAutoSend.AutoSize = true;
+            this.cbxAutoSend.Enabled = false;
+            this.cbxAutoSend.Location = new System.Drawing.Point(387, 88);
+            this.cbxAutoSend.Name = "cbxAutoSend";
+            this.cbxAutoSend.Size = new System.Drawing.Size(74, 17);
+            this.cbxAutoSend.TabIndex = 4;
+            this.cbxAutoSend.Text = "Auto send";
+            this.cbxAutoSend.UseVisualStyleBackColor = true;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(467, 84);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // cmbMididev
+            // 
+            this.cmbMididev.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMididev.FormattingEnabled = true;
+            this.cmbMididev.Location = new System.Drawing.Point(27, 84);
+            this.cmbMididev.Name = "cmbMididev";
+            this.cmbMididev.Size = new System.Drawing.Size(251, 21);
+            this.cmbMididev.TabIndex = 6;
+            this.cmbMididev.SelectedIndexChanged += new System.EventHandler(this.cmbMididev_SelectedIndexChanged);
+            // 
+            // llblSysEx
+            // 
+            this.llblSysEx.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.llblSysEx.Location = new System.Drawing.Point(27, 56);
+            this.llblSysEx.Name = "llblSysEx";
+            this.llblSysEx.Size = new System.Drawing.Size(515, 21);
+            this.llblSysEx.TabIndex = 7;
+            this.llblSysEx.TabStop = true;
+            this.llblSysEx.Text = "llblSysEx";
+            this.llblSysEx.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.llblSysEx.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llblSysEx_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 122);
+            this.Controls.Add(this.llblSysEx);
+            this.Controls.Add(this.cmbMididev);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.cbxAutoSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numChan);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numChan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numChan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbxAutoSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ComboBox cmbMididev;
+        private System.Windows.Forms.LinkLabel llblSysEx;
+    }
+}
+
---
+++ integraTextSender/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Interaction;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Multimedia;
+
+namespace integraTextSender
+{
+    public partial class Form1 : Form
+    {
+        private OutputDevice outputDeviceA = null;
+        private string lasttxt = string.Empty;
+        private bool isInitstate = true;
+        public Form1()
+        {
+            InitializeComponent();
+            cmbMididev.SelectedIndex = -1;
+        }
+
+        private byte checksum(byte[] data)
+        {
+            uint sum = 0;
+            foreach(byte b in data)
+            {
+                sum = sum + b;
+            }
+            return (byte)(128 - (sum % 128)); 
+        }
+
+        private void sendText(string txt, byte channel)
+        {
+            string sendtxt = txt.PadRight(12, ' ');
+            List<byte> data = new List<byte>();
+            byte[] head = { 0x41, 0x10, 0, 0, 0x64, 0x12 };
+            byte[] chan = new byte[2];
+            switch (channel)
+            {
+                case 1:
+                    chan = new byte[] { 0x19, 0x02 };
+                    break;
+                case 2:
+                    chan = new byte[] { 0x19, 0x22 };
+                    break ;
+                case 3:
+                    chan = new byte[] { 0x19, 0x42 };
+                    break;
+                case 4:
+                    chan = new byte[] { 0x19, 0x62 };
+                    break;
+                case 5:
+                    chan = new byte[] { 0x1A, 0x02 };
+                    break;
+                case 6:
+                    chan = new byte[] { 0x1A, 0x22 };
+                    break;
+                case 7:
+                    chan = new byte[] { 0x1A, 0x42 };
+                    break;
+                case 8:
+                    chan = new byte[] { 0x1A, 0x60 };
+                    break;
+                case 9:
+                    chan = new byte[] { 0x1B, 0x02 };
+                    break;
+                case 10:
+                    chan = new byte[] { 0x1B, 0x23 };
+                    break;
+                case 11:
+                    chan = new byte[] { 0x1B, 0x43 };
+                    break;
+                case 12:
+                    chan = new byte[] { 0x1B, 0x62 };
+                    break;
+                case 13:
+                    chan = new byte[] { 0x1C, 0x02 };
+                    break;
+                case 14:
+                    chan = new byte[] { 0x1C, 0x21 };
+                    break;
+                case 15:
+                    chan = new byte[] { 0x1C, 0x41 };
+                    break;
+                case 16:
+                    chan = new byte[] { 0x1C, 0x61 };
+                    break;
+                default:
+                    throw new Exception("invalid midi channel");
+            }    
+            
+            data.AddRange(chan);
+            data.Add(0x00);
+            data.Add(0x00);
+            data.AddRange(Encoding.ASCII.GetBytes(sendtxt));
+            data.Add(checksum(data.ToArray()));
+            data.Add(0xF7);
+            data.InsertRange(0, head);
+
+            outputDeviceA.SendEvent(new NormalSysExEvent(data.ToArray()));
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("F0 ");
+            foreach(byte b in data)
+            {
+                sb.Append($" {b.ToString("X2")}");
+            }
+            llblSysEx.Text = sb.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            cmbMididev.DataSource = OutputDevice.GetAll();
+            cmbMididev.DisplayMember = "Name";
+            cmbMididev.SelectedIndex = -1;
+            //cmbMididev.Text = "-Select midi device-";
+            isInitstate = false;
+        }
+
+        private void cmbMididev_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(!isInitstate)
+            {
+                try
+                {
+                    outputDeviceA = (OutputDevice)cmbMididev.SelectedItem;
+                    numChan.Enabled = true;
+                    tbxText.Enabled = true;
+                    cbxAutoSend.Enabled = true;
+                    btnSend.Enabled = true;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show($"Failed to open midi device{Environment.NewLine}{ex}","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+                
+            }
+        }
+
+        private void tbxText_TextChanged(object sender, EventArgs e)
+        {
+            
+            if(tbxText.Text.Length > 12)
+            {
+                tbxText.Text = lasttxt;
+                tbxText.SelectionStart = tbxText.TextLength;
+                tbxText.SelectionLength = 0;
+            }
+            else
+            {
+
+                lasttxt = tbxText.Text;
+            }
+
+            if (cbxAutoSend.Checked)
+            {
+                sendText(tbxText.Text, (byte)numChan.Value);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            sendText(tbxText.Text, (byte)numChan.Value);
+        }
+
+        private void llblSysEx_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Clipboard.SetText(llblSysEx.Text);
+        }
+    }
+}
